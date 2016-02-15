a6af508cedd5d486b73592615ddac6c35c2397cc
ashuaeron <ashuaeron.nit@gmail.com>
2016-02-15 15:24:08 -0700

    An Example file to commit.

---
+++ IOTDevelopment/src/com/iot/development/IOTDevelopment.java
@@ -0,0 +1,19 @@
+package com.iot.development;
+public class IOTDevelopment {
+
+  static {
+    try {
+      System.loadLibrary("mraajava");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println(
+        "Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" +
+        e);
+      System.exit(1);
+    }
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+  }
+
+}
